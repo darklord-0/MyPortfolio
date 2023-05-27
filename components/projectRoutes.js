9ef69ff944c9cb16cc397ed105e492627e3ecebb
darklord-0 <singhharshit01010@gmail.com>
2023-05-27 22:36:13 +0530

    project and skills images added

---
+++ components/projectRoutes.js
@@ -0,0 +1,38 @@
+export const projectRoutes = [
+    {
+      id:1,
+      name: 'Automated NLP based Search System for Files',
+      slug:'autonbs',
+      description:`Automated NLP based Search System for Files (AutoNBS), an offline multimedia search system designed to fetch relevant files based on user input. AutoNBS utilizes content-based file retrieval techniques and takes into consideration the user's search history to provide personalized and relevant results.`,
+      technologies:'Python,NLP,PyQt5',
+      image:'/../public/assets/projects/autonbs.png',
+      url:'https://github.com/harsha0571/Automated_NLP_Based_Data_Handler'
+    },
+    {
+      id:2,
+      name: 'Graphical Authentication System',
+      slug:'grauth',
+      description:`Visual-based authentication using correct sequence and pixel positions in images aiming to minimize storage issue and shoulder surfing.`,
+      technologies:'React,JavaScript,Express,MongoDB,MaterialUI',
+      image:'/../public/assets/projects/grauth.jpg',
+      url:'https://github.com/AdityaRaj1440/SIH_GraphicalPassword'
+    },
+    {
+      id:3,
+      name: 'Anime Information System',
+      slug:'anime',
+      description:`An API that fetches data related to Anime, Web Novels, Manga, Music,etc. based on the searching/filtering done by the user. `,
+      technologies:'React,JavaScript,Express,MySql',
+      image:'/../public/assets/projects/anime.jpg',
+      url:'https://github.com/AdityaRaj1440/DB_miniproj'
+    },
+    {
+      id:4,
+      name: 'Chat App',
+      slug:'chat',
+      description:'A Simple Chat application where multiple user can exchange messages in real time.',
+      technologies:'NodeJS,JavaScript',
+      image:'/../public/assets/projects/chat.png',
+      url:'https://github.com/darklord-0/MyChat'
+    }
+  ];
